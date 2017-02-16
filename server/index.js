dae782c0b629452194589931019b7eeac844c56c
Draltan Marin <draltan.marin@gmail.com>
2017-02-16 15:50:06 +0100

    feat: basic app setup

--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
 import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import router from './router';
+import notFound from './notFound';
 
 const app = express();
 
-app.use(
-  (req, res) => {
-    res.send('hello');
-  }
-);
+// Basic server setup: log, parse request body, metho
+const loggerFormat = process.env.NODE_ENV == 'production' ?
+'combined': 'dev';
 
-const port = 3000;
+app.use(logger(loggerFormat));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// Let router handle everything. Everything else is not found.
+app.use('/', router);
+app.use(notFound);
+
+const port = process.env.PORT || 3000;
 const server = app.listen(
   port,
   () => {
